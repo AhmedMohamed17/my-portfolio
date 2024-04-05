ace4a7cf75587d3f5483b05288a494a523dbf057
ahmedmohamed17 <ahmedmohamed7sayed@gmail.com>
2024-04-06 01:02:02 +0200

    i'm updated some projects again :fire::fire::fire::fire::fire:

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,8 +1,9 @@
 import ProBox from "./ProBox";
-import ahmed from "../images/ahmed.jpg";
 import todo from "../images/todo.jpg";
 import twitter from "../images/twitter.jpg";
 import movieapp from "../images/movieapp.jpg";
+import inta from "../images/insta.jpg";
+import lego from "../images/lego.jpg";
 
 import React from "react";
 
@@ -16,45 +17,34 @@ function Projects() {
             <h3>Each project is a unique piece of development 🧩</h3>
             <div className="projects-grid">
               <ProBox
-                title="Car Rental"
-                img={ahmed}
-                description="A car rental website is an online platform that allows users to rent cars for personal or business use. The website provides an interface for searching, comparing, and reserving cars."
-                techno1="React"
-                techno2="SCSS"
-                code="https://github.com/stefvndev/car-rental"
-                demo="https://car-rental-ten.vercel.app/"
+                title="Blog Site"
+                img={lego}
+                description="a MERN stack-powered blog site inspired by Lego. Immerse yourself in a world of creativity and imagination where you can explore captivating articles.
+                (Please note that this website is still under construction)"
+                techno1="MERN"
+                techno2="STACK"
+                code="https://github.com/AhmedMohamed17/Blog-App"
+                demo=""
               />
 
               <ProBox
-                title="Movie-App"
-                img={movieapp}
-                description="Introducing our latest project: a website showcasing the newest movies and TV series. Dive into a world of entertainment where you can discover the hottest releases, explore detailed information about each title."
-                techno1="Vanilla JS"
+                title="Instagram Clone"
+                img={inta}
+                description="A simulated Instagram website. Dive into the world of social media where you can discover the latest trends, explore captivating images and videos, connect with friends and influencers."
+                techno1="JQuery"
                 techno2="CSS"
-                code="https://github.com/AhmedMohamed17/Movie-App"
-                demo="https://ahmedmohamed17.github.io/Movie-App/"
+                code="https://github.com/AhmedMohamed17/Fake-instagram"
+                demo="https://nimble-gumption-c941c1.netlify.app/"
               />
-
-              <ProBox
-                title="Movie-App"
-                img={movieapp}
-                description="Introducing our latest project: a website showcasing the newest movies and TV series. Dive into a world of entertainment where you can discover the hottest releases, explore detailed information about each title."
-                techno1="Vanilla JS"
-                techno2="CSS"
-                code="https://github.com/AhmedMohamed17/Movie-App"
-                demo="https://ahmedmohamed17.github.io/Movie-App/"
-              />
-
               <ProBox
                 title="Twitter clone"
                 img={twitter}
-                description="Introducing Fake Twitter Project: A simulated social media platform designed to mimic the functionality and user experience of the popular microblogging site."
+                description="A simulated social media platform designed to mimic the functionality and user experience of the popular microblogging site."
                 techno1="Vanilla JS"
                 techno2="CSS"
                 code="https://github.com/AhmedMohamed17/Twitter"
                 demo="https://fake-twitter-ten.vercel.app/"
               />
-
               <ProBox
                 title="TO-DO List"
                 img={todo}
@@ -63,6 +53,15 @@ function Projects() {
                 techno2="Hooks"
                 code="https://github.com/AhmedMohamed17/To-Do-list"
                 demo="https://delicate-crisp-9ec82b.netlify.app/"
+              />
+              <ProBox
+                title="Movie-App"
+                img={movieapp}
+                description="A website showcasing the newest movies and TV series. Dive into a world of entertainment where you can discover the hottest releases, explore detailed information about each title."
+                techno1="Vanilla JS"
+                techno2="CSS"
+                code="https://github.com/AhmedMohamed17/Movie-App"
+                demo="https://ahmedmohamed17.github.io/Movie-App/"
               />
             </div>
           </div>
